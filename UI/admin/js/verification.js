448b9c20e3b12396e9c3a1bd1ea9b37de065a772
VianneyR4 <vianneyrwicha2017@gmail.com>
2019-12-10 00:29:21 +0200

    Add the admin features

---
+++ UI/admin/js/verification.js
@@ -0,0 +1,13 @@
+// let validation = () => {
+//     var expressionReguliere = /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+//     document.getElementById(resultat).innerHTML = ;
+//     if (expressionReguliere.test(document.getElementById(email).value)) {
+//         document.getElementById(resultat).innerHTML = L 'adresse mail est valide;
+//         document.getElementById(resultat).style.color = green;
+//     } else {
+//         document.getElementById(resultat).innerHTML = L 'adresse mail n'
+//         est pas valide;
+//         document.getElementById(resultat).style.color = red;
+//     }
+//     return false;
+// };
